84b9ad53b1eecef2728a21f68dc8fee76ed1805e
techorionai <david@techorionai.com>
2025-08-19 22:13:44 +0530

    feat: headings config support

--- react-app/src/components/screens/Home/Headings/index.tsx
+++ react-app/src/components/screens/Home/Headings/index.tsx
@@ -1,13 +1,15 @@
 import { Box, Title } from "@mantine/core";
+import { useConfig } from "../../../../providers/ConfigProvider";
 
 export default function HomeHeadings() {
+  const { config } = useConfig();
   return (
     <Box mt="4rem">
-      <Title order={1} fz="h2">
-        Hello👋
+      <Title order={1} fz="h2" mb="xs">
+        {config.homeScreenConfig?.heading ?? "Hello👋"}
       </Title>
       <Title order={2} fz="h2">
-        How may we help you?
+        {config.homeScreenConfig?.heading2 ?? "How may we help you?"}
       </Title>
     </Box>
   );
