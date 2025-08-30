15637bcbc85ec32689d9253037e07be840bcc952
techorionai <david@techorionai.com>
2025-08-30 16:29:23 +0530

    feat: customization options for home screen and navigation bar

--- react-app/src/components/NavFooter/index.tsx
+++ react-app/src/components/NavFooter/index.tsx
@@ -2,11 +2,13 @@ import { Box, Group, Paper, Text, ThemeIcon } from "@mantine/core";
 import { IconHome2, IconMessageChatbot } from "@tabler/icons-react";
 import { NavLink, useLocation } from "react-router";
 import useConfigColors from "../../hooks/useConfigColors";
+import { useConfig } from "../../providers/ConfigProvider";
 
 export const NavFooterHeight = 101;
 
 export default function NavFooter() {
   const location = useLocation();
+  const { config } = useConfig();
   const { primaryColor } = useConfigColors();
 
   const isHomeActive = location.pathname === "/";
@@ -16,7 +18,7 @@ export default function NavFooter() {
     <Paper
       shadow="md"
       withBorder
-      py="md"
+      py="xs"
       px="xl"
       pos="fixed"
       radius="0px"
@@ -24,19 +26,28 @@ export default function NavFooter() {
       left={0}
       right={0}
       style={{ borderLeft: "none", borderRight: "none", borderBottom: "none" }}
+      {...config.footerConfig?.containerProps}
     >
-      <Group justify="center" gap="7rem">
+      <Group justify="center" gap="7rem" pos="relative" top="-4px">
         <NavLink to="/">
           <Box ta="center">
             <ThemeIcon
               variant="transparent"
               color={isHomeActive ? undefined : "gray"}
-              size="xl"
+              size="2.5rem"
+              pos="relative"
+              top="8px"
+              {...config.footerConfig?.home?.iconProps}
             >
-              <IconHome2 size="70%" />
+              <IconHome2 size="100%" />
             </ThemeIcon>
-            <Text fw="bold" fz="sm" c={isHomeActive ? primaryColor : "gray"}>
-              Home
+            <Text
+              fw="bold"
+              fz="sm"
+              c={isHomeActive ? primaryColor : "gray"}
+              {...config.footerConfig?.home?.props}
+            >
+              {config.footerConfig?.home?.text || "Home"}
             </Text>
           </Box>
         </NavLink>
@@ -45,16 +56,20 @@ export default function NavFooter() {
             <ThemeIcon
               variant="transparent"
               color={isSessionsActive ? undefined : "gray"}
-              size="xl"
+              size="2.5rem"
+              pos="relative"
+              top="8px"
+              {...config.footerConfig?.messages?.iconProps}
             >
-              <IconMessageChatbot size="70%" />
+              <IconMessageChatbot size="100%" />
             </ThemeIcon>
             <Text
               fw="bold"
               fz="sm"
               c={isSessionsActive ? primaryColor : "gray"}
+              {...config.footerConfig?.messages?.props}
             >
-              Messages
+              {config.footerConfig?.messages?.text || "Messages"}
             </Text>
           </Box>
         </NavLink>
--- react-app/src/components/screens/Home/Header/index.tsx
+++ react-app/src/components/screens/Home/Header/index.tsx
@@ -3,14 +3,17 @@ import HomeHeaderAvatar from "./Avatar";
 import HomeHeaderCloseButton from "./CloseButton";
 import HomeHeaderLogo from "./Logo";
 import { AI_CHAT_WINDOW_HOME_HEADER_ID } from "../../../../consts/elementIds";
+import { useConfig } from "../../../../providers/ConfigProvider";
 
 export default function HomeHeader() {
+  const { config } = useConfig();
   return (
     <Group
       justify="space-between"
       align="center"
       id={AI_CHAT_WINDOW_HOME_HEADER_ID}
       mb="4rem"
+      {...config.homeScreenConfig?.headerContainerProps}
     >
       <Group>
         <HomeHeaderLogo />
