dbe2e0ed3a9fbd8f571fe897b92d8987e8837bfe
techorionai <david@techorionai.com>
2025-09-14 15:44:54 +0530

    chore: bump version to 0.9.7

--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router";
 
 const root = createRoot(document.getElementById("root")!);
 root.render(
-  <BrowserRouter basename="/sdk-web/0.9.5/app">
+  <BrowserRouter basename="/sdk-web/0.9.7/app">
     <App />
   </BrowserRouter>
 );
