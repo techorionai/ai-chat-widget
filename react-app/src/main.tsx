2acfb66604f4c27cfeba6d6deeb0fa7d825c48bc
techorionai <david@techorionai.com>
2025-09-27 21:46:25 +0530

    chore: bump version

--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router";
 
 const root = createRoot(document.getElementById("root")!);
 root.render(
-  <BrowserRouter basename="/sdk-web/0.9.9/app">
+  <BrowserRouter basename="/sdk-web/0.9.10/app">
     <App />
   </BrowserRouter>
 );
--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -1,6 +1,6 @@
 // vite.config.js
 const version = process.env.VERSION;
-const base = version ? `/sdk-web/${version}/app/` : "/sdk-web/0.9.9/app/";
+const base = version ? `/sdk-web/${version}/app/` : "/sdk-web/0.9.10/app/";
 
 module.exports = {
   base,
