6aeba82ee6ce29f2f62ed589525f8b7c24f1b4bb
techorionai <david@techorionai.com>
2025-09-27 21:30:58 +0530

    chore: bump version

--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router";
 
 const root = createRoot(document.getElementById("root")!);
 root.render(
-  <BrowserRouter basename="/sdk-web/0.9.7/app">
+  <BrowserRouter basename="/sdk-web/0.9.8/app">
     <App />
   </BrowserRouter>
 );
